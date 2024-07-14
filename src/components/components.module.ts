a5601ea24ec919a4c49d66569cf438e876e11ab3
sundjerbob <tadijasimic999@gmai.com>
2024-07-14 02:40:07 +0200

    title

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -5,6 +5,7 @@ import {TopNavigationComponent} from "./navigation/top-navigation.component";
 import {BillboardComponent} from "./billboard/billboard.component";
 import {TextWriterComponent} from "./text-billboard/text-writer/text-writer.component";
 import {TextBillboardComponent} from "./text-billboard/text-billboard.component";
+import {ReservationSearchComponent} from './reservation-search/reservation-search/reservation-search.component';
 
 @NgModule({
   declarations: [
@@ -12,15 +13,17 @@ import {TextBillboardComponent} from "./text-billboard/text-billboard.component"
     TopNavigationComponent,
     BillboardComponent,
     TextWriterComponent,
-    TextBillboardComponent
+    TextBillboardComponent,
+    ReservationSearchComponent
   ],
-    imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule, NgOptimizedImage],
   exports: [
     LightSwitchComponent,
     TopNavigationComponent,
     BillboardComponent,
     TextWriterComponent,
-    TextBillboardComponent
+    TextBillboardComponent,
+    ReservationSearchComponent
   ]
 })
 
---
+++ src/components/reservation-search/reservation-search/reservation-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-search',
+  templateUrl: './reservation-search.component.html',
+  styleUrl: './reservation-search.component.scss'
+})
+export class ReservationSearchComponent {
+
+}
