3da0707573d3d1254c9c12eec6724e5141c9a0e6
sundjerbob <tadijasimic999@gmai.com>
2024-07-14 19:40:46 +0200

    starting to look like smt...

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -5,7 +5,7 @@ import {TopNavigationComponent} from "./navigation/top-navigation.component";
 import {BillboardComponent} from "./billboard/billboard.component";
 import {TextWriterComponent} from "./text-billboard/text-writer/text-writer.component";
 import {TextBillboardComponent} from "./text-billboard/text-billboard.component";
-import {ReservationSearchComponent} from './reservation-search/reservation-search/reservation-search.component';
+import {ReservationSearchComponent} from './reservation-search/reservation-search.component';
 
 @NgModule({
   declarations: [
